76a8eafe834f943a18d4df12cb824395d3733844
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-19 16:45:51 +0530

    installs and integrates font-awesome with yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 import Rails from "@rails/ujs"
 import "bootstrap"
+import "@fortawesome/fontawesome-free/css/all"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
