8df7c1dd44cdf36a85e294e380e00d5eacb6cc74
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-19 15:20:05 +0530

    modifies bootstrap configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  // uncomment below for bootstrap 4.x
+  // Popper: ['popper.js', 'default']
+  // uncomment below for bootstrap 5
+  Popper: ['@popperjs/core', 'default']
+}))
+
 module.exports = environment
