1bbbd982fd3648d2ff7826208a021f2ffb45eb1f
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-21 09:12:42 +0530

    Adds Users Index and show actions..

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'users/confirmations'}
+  resources :users, only: [:index, :show]
   root 'pages#home'
   get 'privacy', to: 'pages#privacy_policy'
 end
