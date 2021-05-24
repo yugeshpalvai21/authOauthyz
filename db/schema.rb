5f5a0dc27b80c2adbbcd1dcfd60184ed780ac962
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-25 00:38:09 +0530

    generates new migration to add roles column with type jsonb to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_082034) do
+ActiveRecord::Schema.define(version: 2021_05_24_190316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,11 @@ ActiveRecord::Schema.define(version: 2021_05_24_082034) do
     t.inet "last_sign_in_ip"
     t.string "provider"
     t.string "uid"
+    t.jsonb "roles", default: {}, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["roles"], name: "index_users_on_roles", using: :gin
   end
 
 end
