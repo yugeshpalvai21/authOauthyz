e02fd71e3882c1644c9df7a790302654b577cad4
Yugesh Palvai <yugeshpalvai21@gmail.com>
2021-05-25 20:58:33 +0530

    genrates migration for invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :trackable, :omniauthable,
+  devise :invitable, :database_authenticatable, :registerable, :confirmable, :trackable, :omniauthable,
          :recoverable, :rememberable, :validatable
 
   include Roleable
